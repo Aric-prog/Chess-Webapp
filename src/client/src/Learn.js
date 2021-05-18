7c9e2db3caa4a21c94802c5fc496816c6c4a0e02
BryanPutra <bpsuandi@gmail.com>
2021-05-18 19:08:46 +0700

    make chess pieces data dynamic

--- src/client/src/Learn.js
+++ src/client/src/Learn.js
@@ -4,14 +4,60 @@
 // import bishop from './images/bishop.png'
 // import rook from './images/rook.png'
 // import knight from './images/knight.png'
+import { useState } from 'react'
 const Learn = () => {
+
+    const [pieces, setPieces] = useState([
+        {
+            name: 'King',
+            icon: 'fas fa-chess-king',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        },
+        {
+            name: 'Queen',
+            icon: 'fas fa-chess-queen',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        },
+        {
+            name: 'Rook',
+            icon: 'fas fa-chess-rook',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        },
+        {
+            name: 'Knight',
+            icon: 'fas fa-chess-knight',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        },
+        {
+            name: 'Bishop',
+            icon: 'fas fa-chess-bishop',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        },
+        {
+            name: 'Pawn',
+            icon: 'fas fa-chess-pawn',
+            image2: '',
+            description: 'lorem ipsum..... lorem ipsum',
+        }
+    ]);
 
     return (
         <div className="rules-wrapper">
             <div className="container">
                 <h1>The Rules Of Chess</h1>
                 <div className="piece-container">
-                    <div className="piece king">
+                    {pieces.map((piece) => (
+                        <div className="piece">
+                            <i className={piece.icon}></i>
+                            <h1>{piece.name}</h1>
+                        </div>
+                    ))}
+                    {/* <div className="piece king">
                         <i class="fas fa-chess-king"></i>
                         <h1>King</h1>
                     </div>
@@ -34,7 +80,7 @@ const Learn = () => {
                     <div className="piece pawn">
                         <i class="fas fa-chess-pawn"></i>
                         <h1>Pawn</h1>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
