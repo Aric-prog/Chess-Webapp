21a339d37ecce9e255f4d5bb348e3650c6720925
Aric <hernandoaric@gmail.com>
2021-05-18 22:25:24 +0700

    added temporary files for board

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,13 +1,14 @@
 const { default: Header } = require("./Header");
 const { default: Home } = require("./Home");
 const { default: Learn } = require("./Learn");
+const { default: Play} = require("./Play")
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      {/* <Home></Home> */}
-      <Learn></Learn>
+      <Home></Home>
+      {/* <Play></Play> */}
     </div>
   );
 }
---
+++ src/client/src/Play.js
@@ -0,0 +1,11 @@
+import WithMoveValidation from "./board"
+const Play = () => {
+    
+    return (
+        <div className="board">
+            <WithMoveValidation/>
+        </div>
+    );
+}
+
+export default Play
