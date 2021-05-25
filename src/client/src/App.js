7b99fcb135b27ea55c11ab2d7465c9e85846070b
BryanPutra <bpsuandi@gmail.com>
2021-05-25 14:34:20 +0700

    added routes

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,20 +1,40 @@
-import LoginModal from "./LoginModal";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const { default: Header } = require("./Header");
 const { default: Home } = require("./Home");
 const { default: Learn } = require("./Learn");
-const { default: Play} = require("./Play")
+const { default: Play } = require("./Play")
+const { default: About } = require("./About");
+const { default: LoginModal } = require("./LoginModal");
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      {/* <Home></Home> */}
-      {/* <LoginModal></LoginModal> */}
-      {/* <Learn></Learn> */}
-      <Play></Play>
-      {/* <test></test> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+        <div className="main-content">
+          <Switch>
+            <Route exact path="/home">
+              <Home></Home>
+            </Route>
+            <Route exact path="/learn">
+              <Learn></Learn>
+            </Route>
+            <Route exact path="/play"> 
+              <Play></Play>
+            </Route>
+            {/* <Route exact path="/about">
+              <About></About>
+            </Route> */}
+          </Switch>
+
+          {/* <LoginModal></LoginModal> */}
+          {/* <Learn></Learn> */}
+          {/* <Play></Play> */}
+          {/* <test></test> */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/client/src/Header.js
+++ src/client/src/Header.js
@@ -1,24 +1,31 @@
 import { useState } from 'react';
+import LoginModal from './LoginModal';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
     const linkTemp = "#";
-
 
     return (
         <nav className="navbar">
             <div className="container">
                 <div className="header-left">
                     <ul className="header-left-list">
-                        <li><a href={linkTemp} className="logo" ><i class="fas fa-chess-board"></i> Chess</a></li>
+                        {/* <li ><a href={linkTemp} className="logo" ><i class="fas fa-chess-board"></i> Chess</a></li>
                         <li ><a href={linkTemp} className="learn" >Learn</a></li>
                         <li ><a href={linkTemp} className="play" >Play</a></li>
-                        <li ><a href={linkTemp} className="about-us" >About Us</a></li>
+                        <li ><a href={linkTemp} className="about-us" >About Us</a></li> */}
+                        <li><Link to="/home" className="logo"> <i class="fas fa-chess-board"></i> Chess</Link></li>
+                        <li><Link to="/learn" className="learn"> Learn</Link></li>
+                        <li><Link to="/play" className="play"> Play</Link></li>
+                        <li><Link to="/about-us" className="about-us"> About Us</Link></li>
                     </ul>
                 </div>
                 <div className="header-right">
                     <div className="sign-container">
-                        <a href={linkTemp} className="signin">Sign in</a>
+                        <a href={linkTemp} className="signin" onClick={() => {return(
+                            <LoginModal></LoginModal>
+                        )}}>Sign in</a>
                         <a href={linkTemp} className="signup">Sign up</a>    
                     </div>
                 </div>
