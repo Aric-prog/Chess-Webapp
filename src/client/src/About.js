e64bed05f986d3d9ee7910b92afef75b2b870ec5
BryanPutra <bpsuandi@gmail.com>
2021-06-15 05:03:02 +0700

    styled about us page

--- src/client/src/About.js
+++ src/client/src/About.js
@@ -3,6 +3,11 @@ import Members from './Members';
 import bryan from './images/bepe.jpg'
 import aric from './images/aric.jpg'
 import jocelyn from './images/joce.jpg'
+import reactLogo from './images/reactjs-yellow.svg'
+import socketIoLogo from './images/socketio-yellow.svg'
+import nodeJsLogo from './images/nodejs-yellow.svg'
+import fireBaseLogo from './images/firebase.svg'
+import websiteIllus from './images/website2.svg'
 
 const About = () => {
 
@@ -31,8 +36,28 @@ const About = () => {
                 <div className="thanks-wrapper">
                     <h1>THANKS FOR PLAYING!</h1>
                     <p>This was a project that we made for our Web Application Development and Security course. It involves several technologies and framework such as React, Nodejs, Socket.io, and Firebase.
-                        
                     </p>
+                </div>
+                <div className="frameworks-container">
+                    <img className="website-image" src={websiteIllus} alt="bruh" />
+                    <div className="framework-container">
+                        <div className="framework">
+                            <img src={reactLogo} alt=""/>
+                            <h3>ReactJs</h3>
+                        </div>
+                        <div className="framework">
+                            <img src={fireBaseLogo} alt=""/>
+                            <h3>Firebase</h3>
+                        </div>
+                        <div className="framework">
+                            <img src={socketIoLogo} alt="" />
+                            <h3>SocketIo</h3>
+                        </div>
+                        <div className="framework">
+                            <img src={nodeJsLogo} alt="" />
+                            <h3>NodeJs</h3>
+                        </div>
+                    </div>
                 </div>
                 <div className="member-wrapper">
                     <Members members={members}></Members>
--- src/client/src/Home.js
+++ src/client/src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
                 <div className="content-wrapper">
                     <div className="home-content-left">
                         <div className="text-wrapper">
-                            <h1>Chess is officially an e-sport (lol?)</h1>
+                            <h1>Chess is officially an e-sport</h1>
                             <h3>Don't have a board? Play chess online here for free! Compete with strangers or friends alike to test your skills!</h3>
                         </div>
                         <div className="btn-wrapper">
